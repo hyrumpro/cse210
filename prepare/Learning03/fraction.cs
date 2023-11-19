2d66638ee95d0cdae4e34093d38956010e1b0a78
Hyrum Perez <hyrumpa@gmail.com>
2023-11-18 19:06:17 -0500

    Learning 03 Activity update

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,58 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom != 0 ? bottom : 1; // Ensuring denominator is not zero
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+            _bottom = bottom;
+        else
+            Console.WriteLine("Denominator cannot be zero.");
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
